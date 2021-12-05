9214467b008417c55aa3761fa295adab6f2eb3fd
Timgo-1 <timgodesigns@gmail.com>
2021-12-05 11:23:16 +0300

    FIirst Commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  reactStrictMode: true,
+  images:{
+    domains:['bayut-production.s3.eu-central-1.amazonaws.com']
+},
+  env: {
+    NEXT_PUBLIC_RAPID_API_KEY: '7d8e70601fmshac981762c29e663p154bbejsn5443d5f72e1b',
+},
+}
